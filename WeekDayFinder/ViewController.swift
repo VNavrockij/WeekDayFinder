04815f90185eefd5f9836da38f9dd8b079007786
VNavrockij <vitalik.nav@gmail.com>
2022-04-21 13:03:36 +0300

    add: some constraints

--- WeekDayFinder/ViewController.swift
+++ WeekDayFinder/ViewController.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    
+    @IBOutlet weak var resultLable: UILabel!
+    
+    @IBAction func findDay() {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
